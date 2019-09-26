337b08a2b2479cd4415c0e0d6cd9bdbdb2874bdc
Vladimir Jovanović <vladimir@bitersen.com>
2019-09-26 12:48:58 +0200

    Change export to JS class
    - Add file extension checking
    
    Affects #1

--- index.js
+++ index.js
@@ -4,34 +4,71 @@ const compileScss = require('./src/js/compileScss')
 const msg = require('bit-message-box')
 const fs = require('fs-extra')
 const path = require('path')
+const chalk = require('chalk')
 
-module.exports = (options = {}) => {
-  // Set default options
-  options = {
-    breakpointsJson: './src/breakpoints.json',
-    input: '',
-    output: '',
-    ...options
+class BreakpointSurfer {
+  constructor (options = {}) {
+    this._options = {
+      breakpointsJson: './src/breakpoints.json',
+      input: '',
+      output: '',
+      ...options
+    }
   }
 
-  breakpointMap(options.breakpointsJson)
-    .then((data) => {
-      fs.outputFile(path.join(__dirname, './generated/_resolutions.scss'), data)
-        .then(() => {
-          if (options.input !== '' && options.output !== '') {
+  // Error checking
+  // --------------
+  // Check for correct extensions
+  _extensionCheck (file, expectedExtension) {
+    if (path.extname(file).toLowerCase() !== `.${expectedExtension}`) {
+      msg.error(`ERROR: File ${chalk.yellow(path.basename(file))} must have ${chalk.yellow(expectedExtension)} extension`)
+
+      return false
+    }
+
+    return true
+  }
+
+  // Complete error check
+  _errorCheck () {
+    if (
+      this._extensionCheck(this._options.breakpointsJson, 'json') &&
+      this._extensionCheck(this._options.input, 'scss') &&
+      this._extensionCheck(this._options.output, 'css')
+    ) { return true } else { return false }
+  }
+
+  // Compile results
+  // ---------------
+  compile () {
+    // Check for errors before continuing
+    if (!this._errorCheck()) { return false }
+
+    // Compile
+    breakpointMap(this._options.breakpointsJson)
+      .then((data) => {
+        // Create scss map with name based on breakpointsJson
+        this._mapName =
+
+        fs.outputFile(path.join(__dirname, './generated/_resolutions.scss'), data)
+          .then(() => {
+            if (this._options.input !== '' && this._options.output !== '') {
             // Generate output
-            compileScss({
-              src: options.input,
-              dest: options.output
-            })
+              compileScss({
+                src: this._options.input,
+                dest: this._options.output
+              })
 
-            return true
-          } else {
-            msg.error('File not created! Check your input and output values')
+              return true
+            } else {
+              msg.error('File not created! Check your input and output values')
 
-            return false
-          }
-        })
-        .catch((error) => msg.error(error))
-    })
+              return false
+            }
+          })
+          .catch((error) => msg.error(error))
+      })
+  }
 }
+
+module.exports = BreakpointSurfer
